5b88fb021a8b8d2d59c96e9d1c848b1bf879f5de
김지호 <crsn1111@gmail.com>
2022-02-06 18:46:05 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -270,9 +270,7 @@ async def 재생(ctx, *, msg):
         source = driver.page_source
         bs = bs4.BeautifulSoup(source, 'lxml')
         entire = bs.find_all('a', {'id': 'video-title'})
-        print(entire)
         entireNum = entire[0]
-        print(entireNum)
         entireText = entireNum.text.strip()
         musicurl = entireNum.get('href')
         url = 'https://www.youtube.com'+musicurl
